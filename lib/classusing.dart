84f4df2760b506768d3dfb28006937071571df85
abhishek <abhishekmrjbr@outlook.com>
2023-07-29 02:34:24 +0530

    using class i make all questions and answer together in a same list and here i use class from other file and i import file in main.dart

---
+++ lib/classusing.dart
@@ -0,0 +1,8 @@
+class QuestionBank {
+  late String allquestions;
+  late bool allanswer;
+  QuestionBank({String? q, bool? a}) {
+    allquestions = q!;
+    allanswer = a!;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'classusing.dart';
 
 void main() {
   runApp(quizflutterapp());
@@ -21,7 +22,7 @@ class qfa extends StatefulWidget {
 }
 
 class _qfaState extends State<qfa> {
-  List<Icon> score=[
+  List<Icon> score = [
     /*Icon(Icons.check,
       color: Colors.green,
       //TODO: create icon for wrong or cross
@@ -31,20 +32,25 @@ class _qfaState extends State<qfa> {
       //TODO: create icon for wrong or cross
     ),*/
   ];
-  List<String> questions=[
-
+  /*
+  List<String> questions = [
     'In my Laptop Linux Mint Xfce work fast?',
     'In my Laptop Linux Mint i used for programming?',
     'In my Laptop i used windows mostly?',
   ];
-
-  int i=0;
+  List<bool> answer = [true, true, false];*/
+  List<QuestionBank> qtnanstgr = [
+    QuestionBank(q: 'In my Laptop Linux Mint Xfce work fast?', a: true),
+    QuestionBank(q: 'In my Laptop Linux Mint i used for programming?', a: true),
+    QuestionBank(q: 'In my Laptop i used windows mostly?', a: false)
+  ];
+  int i = 0;
+  /*
   String tquest(int i ){
-    return questions[i];
-  }
+     return questions[i];
+  }*/
   @override
   Widget build(BuildContext context) {
-
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.black87,
@@ -57,82 +63,94 @@ class _qfaState extends State<qfa> {
             ),
             Center(
                 child: Text(
-              tquest(i),
-              style: TextStyle(fontSize: height / 20, color: Colors.white),
+              qtnanstgr[i].allquestions,
+              style: TextStyle(fontSize: height / 27, color: Colors.white),
             )),
             SizedBox(
-              height: height/3,
+              height: height / 3,
             ),
             Expanded(
-
               child: Padding(
                 padding: EdgeInsets.all(15),
-
                 child: TextButton(
-                  
                   onPressed: () {
                     setState(() {
-                      score.add(
-                        Icon(Icons.check,
+                      score.add(Icon(
+                        Icons.check,
                         color: Colors.green,
-                        )
-                      );
-                      i++;
-                      if(i>2){
-                        i=0;
+                      ));
+                      bool answerr = qtnanstgr[i].allanswer;
+                      if (answerr == true) {
+                        print('you won');
+                      } else {
+                        print('you lose');
                       }
-                      tquest(i);
+
+                      i++;
+                      if (i > 2) {
+                        i = 0;
+                      }
+                      qtnanstgr[i].allquestions;
+                      //tquest(i);
                     });
                     print(' i am True');
                   },
-                  child: Text('True',style: TextStyle(
-                    fontSize: 20,
-                  ),),
+                  child: Text(
+                    'True',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
                   style: ButtonStyle(
                     foregroundColor: MaterialStateProperty.all(Colors.white),
                     backgroundColor: MaterialStateProperty.all(Colors.green),
                     //minimumSize: MaterialStateProperty.all(Size.fromWidth(20)),
                   ),
-
                 ),
               ),
             ),
-
             Expanded(
-
               child: Padding(
                 padding: EdgeInsets.all(15),
-
                 child: TextButton(
-
                   onPressed: () {
                     setState(() {
-                      score.add(
-                        Icon(Icons.close,
+                      score.add(Icon(
+                        Icons.close,
                         color: Colors.red,
-                        )
-                      );
+                      ));
+                      bool answeri = qtnanstgr[i].allanswer;
+                      if (answeri == false) {
+                        print('you won');
+                      } else {
+                        print('you lose');
+                      }
+
+                      i++;
+                      if (i > 2) {
+                        i = 0;
+                      }
+                      qtnanstgr[i].allanswer;
                       print('i clicked');
                     });
                   },
-                  child: Text('False',style: TextStyle(
-                    fontSize: 20,
-                  ),),
+                  child: Text(
+                    'False',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
                   style: ButtonStyle(
                     foregroundColor: MaterialStateProperty.all(Colors.white),
                     backgroundColor: MaterialStateProperty.all(Colors.red),
                     //minimumSize: MaterialStateProperty.all(Size.fromWidth(20)),
                   ),
-
                 ),
               ),
             ),
-
             Row(
               children: score,
             ),
-
-
           ],
         ),
       ),
